3cc1db0108096aa1af9c29a2dc4d1a48e1e26983
Daniel Azamar-Chacha <daniel@orenda.local>
2018-10-07 12:59:43 -0500

    Implementando mock de carga de roles

--- backend/src/main/java/com/backend/security/JWTAuthenticationFilter.java
+++ backend/src/main/java/com/backend/security/JWTAuthenticationFilter.java
@@ -1,24 +1,34 @@
 package com.backend.security;
 
-import com.auth0.jwt.JWT;
-import com.backend.entities.Usuario;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
 
-import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.backend.entities.Rol;
+import com.backend.entities.Usuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -37,11 +47,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             Usuario creds = new ObjectMapper()
                     .readValue(req.getInputStream(), Usuario.class);
 
+            
+            Rol rol = new Rol();
+            rol.setRol("ADMIN");
+            
+            List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(rol.getRol());
+            
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             creds.getUsername(),
                             creds.getPassword(),
-                            new ArrayList<>())
+                            grantedAuthorities)
             );
             
         } catch (IOException e) {
@@ -57,8 +73,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
 
+		Claims claims = Jwts.claims()
+                .setSubject(((User) auth.getPrincipal()).getUsername());
+        claims.put("role", "ADMIN");
+        
+        String s = ((User) auth.getPrincipal()).getUsername();
+		
+        System.out.println("USER: " + s  );
+        
         String token = JWT.create()
                 .withSubject(((User) auth.getPrincipal()).getUsername())
+                .withClaim("role", "ADMIN")
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
                 .sign(HMAC512(SecurityConstants.SECRET.getBytes()));
         res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
