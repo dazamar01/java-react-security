713b4ecc160839dfc8673f2349804dda03881375
Daniel Azamar-Chacha <daniel@orenda.local>
2018-10-08 22:22:20 -0500

    Eliminando codigo no funcional

--- api/src/main/java/com/api/services/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.api.services;
-
-import com.api.entities.Usuario;
-
-public interface AuthenticationService {
-
-	public boolean validarLoginUsuario(Usuario usuario) throws Exception;
-	
-}
